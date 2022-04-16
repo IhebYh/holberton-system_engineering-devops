4c4b41aed91c17e7a2112e2c7ccb5733bb8dcc15
Iheb Yahyaoui <iheb.yah@gmail.com>
2022-04-16 16:06:07 +0100

    ssh_config using puppet 'it was fun'

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
 # Turns off pass auth and uses pub key in school file
 file_line { 'set password auth to no':
-ensure  => 'present',
-path    => '/etc/ssh/ssh_config',
-line    => '    PasswordAuthentification no',
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentification no',
 }
+
 file_line { 'Specify Identity the file':
-ensure  => 'present',
-path    => '/etc/ssh/ssh_config',
-line    => '    IdentityFile ~/.ssh/school',
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile ~/.ssh/school',
 }
